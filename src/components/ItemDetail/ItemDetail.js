4eb36c03460f4aa01cade77c95acbc37a8b02e68
Gabrielpompa1 <gabrielpompa@email.arizona.edu>
2020-12-16 00:08:47 -0700

    Item suggestion works. Need to add links to an upcycle item component

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ItemSuggestions from '../ItemSuggestions/ItemSuggestions';
 import Axios from 'axios';
 import './ItemDetail.css';
 
@@ -6,16 +7,14 @@ const ItemDetail = ({ match }) => {
 	//// -- Variables -- ////
 
 	let itemId = match.params.id;
+	// let itemType = match.params.item_type;
 	let itemUrl = `http://localhost:8000/item/${itemId}`;
+
 	//// -- States -- ////
 
 	const [itemInfo, setItemInfo] = useState('');
 
 	//// -- Functions / Event Handlers -- ////
-
-	// const handleSubmit = (event) => {
-	// 	const data = 
-	// }
 
 	//// -- useEffect -- ////
 
@@ -41,12 +40,12 @@ const ItemDetail = ({ match }) => {
 					<h2 className='item-name'>{itemInfo.name}</h2>
 					<div>
 						<h3>{itemInfo.category}</h3>
+						<h3>{itemInfo.item_type}</h3>
 						<h3>
 							{itemInfo.classification} - {itemInfo.condition}
 						</h3>
 						<h3>Description</h3>
 						<p>{itemInfo.description}</p>
-						<div></div>
 					</div>
 				</div>
 				<div className='item-picture-div'>
@@ -58,9 +57,10 @@ const ItemDetail = ({ match }) => {
 					/>
 				</div>
 			</div>
-            <div className='featured-work'>
-                
-            </div>
+			<div className='featured-work'>
+				<h2>Upcycle Suggestions</h2>
+				<ItemSuggestions itemInfo={itemInfo} />
+			</div>
 		</div>
 	);
 };
